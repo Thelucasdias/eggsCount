44404c71c718abf1c5d9fc9910a4ffde8b73fac7
Thelucasdias <llucasdias@yahoo.com.br>
2025-08-21 22:02:29 -0300

    calculations.js

---
+++ src/utils/calculations.js
@@ -0,0 +1,13 @@
+/**
+ * Calculates the positivity rate of traps.
+ *
+ * @param {Array<Object>} data - List of traps with their egg counts.
+ * @param {number} data[].eggs - Number of eggs found in the trap.
+ * @returns {number} - Percentage of traps that are positive (0–100).
+ */
+
+export function calculatePositivityRate(data) {
+  const total = data.length;
+  const positives = data.filter((trap) => trap.eggs > 0).length;
+  return total > 0 ? (positives / total) * 100 : 0;
+}
